6903e0b4f2c1b723be7550922bbda1be71e86370
Paul Snyder <paul@pataprogramming.com>
2014-08-20 14:57:12 -0400

    Single-variable FOR-NEXT loops implemented

--- src/basic/parser.clj
+++ src/basic/parser.clj
@@ -5,7 +5,22 @@
             [clojure.walk :as w]
             [clojure.pprint :as pp]))
 
-(def tt "10 A=1\n20 B=2\n10 C=3\n30 D=4\n50 D=5")
+(def tt "10 A=1
+         20 B=2
+         30 C=3
+         34 GOTO 40
+         35 C=4
+         40 D=4
+         50 D=5
+         60 GOSUB 100
+         99 END
+         100 E=5
+         110 RETURN")
+
+(def uu "10 J=1+2
+         20 FOR I=1 TO J
+         30 PRINT I
+         40 NEXT I")
 
 (defn compare-pair [[a b] [c d]]
   (case (compare a c)
@@ -35,8 +50,10 @@
     statement     = assignment
                   | print
                   | remark
-                  | if-then
                   | if-then-else
+                  | if-then
+                  | for
+                  | next
                   | goto
                   | gosub
                   | return
@@ -49,9 +66,12 @@
     cond-destination = expression
     else-clause   = <'ELSE' <ws>> (cond-destination | statements)
 
-    if            = if-then | if-then-else
     if-then       = <'IF' <ws>> expression <ws> then-clause
     if-then-else  = <'IF' <ws>> expression <ws> then-clause <ws> else-clause
+
+    for           = <'FOR' ws> id <ws*> <'=' ws*> expression <ws 'TO' ws> expression
+                    (<ws 'STEP' ws> expression)?
+    next          = <'NEXT' ws> id
 
     goto          = <'GOTO' <ws>> expression
     gosub         = <'GOSUB' <ws>> expression
@@ -107,7 +127,6 @@
      [(keyword b) a c]))
 
 (defn process-expressions [parse-tree]
-  (println "processing " parse-tree)
   (ip/transform
    {:integer     (comp clojure.edn/read-string str)
     :alphanum    str
@@ -119,147 +138,15 @@
     :add_exp     treeify
     :mult_exp    treeify
     :negate_exp  treeify
-    :power_exp   treeify
-    ;;:statement   (fn stmtify [[action & args]] {:action action :args (vec args)})
-    ;; :statements  (fn sublabel [& statements]
-    ;;                (for [idx (range (count statements))]
-    ;;                  (update-in (nth statements idx)
-    ;;                             [:label] #(conj % (* 10 idx)))))
-    ;;;; :cond-destination
-    ;;;; (fn cond-to-goto [expression]
-    ;;;;   [{:label [0] :action :goto :args [expression]}])
-    ;; :then-clause (fn thenlabel [statements]
-    ;;                (label-clause-level 1 statements))
-    ;; :else-clause (fn elselabel [statements]
-    ;;                (label-clause-level 2 statements))
-    ;; :line        (fn lineify [[_ label] statements]
-    ;;                (println " STATEMENTS:" statements)
-    ;;                (for [idx (range (count statements))]
-    ;;                  (update-in (nth statements idx) [:label] #(conj % label))))
-
-    ;; :program     (fn programmify [& lines]
-    ;;                (reduce (fn [acc {:keys [label] :as line}] (assoc acc label line))
-    ;;                        (avl/sorted-map-by compare-seq)
-    ;;                        (mapcat identity lines)))
-}
+    :power_exp   treeify}
    parse-tree))
 
-
-(defn maybe-statementify [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :statement))
-    (let [[_ [action & args]] t]
-      {:action action :args (vec args)})
-    t))
-
-(defn label-clause-level [level statements]
-  (println "labeling: " statements "with: " level)
-  (for [idx (range (count statements))]
-    (update-in (nth statements idx)
-               [:label] #(conj % level))))
-
-(defn label-subtree-level [level t]
-  (let [label-key (fn [m k] (if (contains? m k)
-                             (update-in m [k]
-                                        #(conj % level))
-                             m))
-        labeller  #(if (map? %)
-                     (-> %
-                         (label-key :label)
-                         (label-key :destination))
-                     %)]
-    (w/postwalk labeller t)))
-
-(defn rewrite-then [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :then-clause))
-    (apply vector :statements (label-clause-level 2 (next (fnext t))))
-    t))
-
-(defn rewrite-else [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :else-clause))
-    (apply vector :statements (label-clause-level 4 (next (fnext t))))
-    t))
-
-(defn maybe-rewrite-ifthen [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :statements)
-           (map? (fnext t))
-           (= (:action (fnext t)) :if-then))
-    (let [[_ {:keys [args]}] t
-          [tst then-stmts] args]
-      [:statements
-       {:action :test-jump :args [tst {:destination '(2)}] :label '(0)}
-       (conj then-stmts {:action :jump :label '(3) :args [{:destination '(5)}]})])
-    t))
-
-(defn maybe-rewrite-ifthenelse [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :statements)
-           (map? (fnext t))
-           (= (:action (fnext t)) :if-then-else))
-    (let [[_ {:keys [args]}] t
-          [tst then-stmts else-stmts] args]
-      [:statements
-       {:action :test-jump :args [tst {:destination '(2)} {:destination '(4)}] :label '(0)}
-       (conj then-stmts {:action :jump :label '(3) :args [{:destination '(5)}]})
-       else-stmts])
-    t))
-
-
-(defn rewrite-statements [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :statements))
-    (let [[_ & rst] t
-          statements (vec rst)]
-      (print "PP") (println statements)
-      (for [idx (range (count statements))]
-        (update-in (nth statements idx)
-                   [:label] #(do
-                               (conj % (* 10 idx))))))
-    t))
-
-;; :statements  (fn sublabel [& statements]
-    ;;                (for [idx (range (count statements))]
-    ;;                  (update-in (nth statements idx)
-    ;;                             [:label] #(conj % (* 10 idx)))))
-
-
-(defn rewrite-line-bad [t]
-  (if (and (coll? t)
-           (not (map? t))
-           (= (first t) :line))
-    (let [[_ [_ label] & rst] t
-          [[_ & statements]]    rst]
-      (pp/pprint statements)
-      [:statements (for [idx (range (count statements))]
-                     (update-in (nth statements idx) [:label] #(conj % label)))])
-    t))
 
 (defn rewrite-cond-destination [t]
   (if (and (vector? t)
            (= (first t) :cond-destination))
     [:statements [:statement [:goto (fnext t)]]]
     t))
-
-(defn insert-second-when [t pred item]
-  (if (pred t)
-    (let [[fst & rst] t]
-      (apply vector fst item rst))
-    t))
-
-(defn label-all-statements [t]
-  (insert-second-when t #(and (vector? %)
-                              (= (first %) :statement)
-                              (not= (first (second %)) :label))
-                      [:label []]))
 
 (defn append-to-labels [label t]
   (if (and (vector? t)
@@ -271,6 +158,16 @@
   (if (and (vector? t)
            (= (first t) :label))
     [:label (apply vector label (second t))]
+    t))
+
+(defn rewrite-if-then [t]
+  (if (and (vector? t)
+           (= (first t) :if-then))
+    (let [[_ tst then-clause] t]
+      [:if-then
+       [:statement [:label [0]] [:test-jump tst [:label [2]]]]
+       [:statement [:label [1]] [:goto [:label [4]]]]
+       then-clause])
     t))
 
 (defn rewrite-if-then-else [t]
@@ -312,17 +209,28 @@
     t
     ))
 
-(defn strip-then-else [t]
+(defn rewrite-for [t]
   (if (and (vector? t)
-           (or (= (first t) :then-clause)
-               (= (first t) :else-clause)
-               ))
+           (= (first t) :for))
+    (let [[_ [_ id] init end-val & [step]] t]
+      [:for
+       [:statement [:label [0]] [:assignment [:id id] init]]
+       [:statement [:label [1]] [:store-for
+                                 [:id id]
+                                 [:label [2]]
+                                 end-val
+                                 (or step [:constant 1])]]])
+    t))
+
+(defn strip-control [t]
+  (if (and (vector? t)
+           (#{:then-clause :else-clause} (first t)))
     (let [[_ statements] t] (vec statements))
     t))
 
 (defn collapse-statement-level [t]
   (if (and (vector? t)
-           (#{:statements :if-then :if-then-else} (first t)))
+           (#{:statements :if-then :if-then-else :for} (first t)))
     (next t)
     [t]))
 
@@ -338,6 +246,7 @@
 
 (defn collapse-nested-statement [t]
   (if (and (vector? t)
+           (>= (count t) 2)
            (vector? (first (subvec t 2)))
            (= (ffirst (subvec t 2)) :statement))
     (nnext t)
@@ -365,7 +274,7 @@
   (if (and (vector? t)
            (= (first t) :statement))
     (let [[_ [_ label] [action & args]] t]
-      {:label label :action action :args args})
+      {:label label :action action :args (vec args)})
     t))
 
 (defn programmify [t]
@@ -375,54 +284,30 @@
               (assoc acc label stmt))
             (avl/sorted-map-by compare-seq)
             (next t))
-    t)
-)
-; :program     (fn programmify [& lines]
-    ;;                (reduce (fn [acc {:keys [label] :as line}] (assoc acc label line))
-    ;;                        (avl/sorted-map-by compare-seq)
-    ;;                        (mapcat identity lines)))
-
+    t))
 
 (defn proc-steps [t]
   (->> t
        (w/postwalk rewrite-cond-destination)
        (w/postwalk rewrite-if-then-else)
+       (w/postwalk rewrite-if-then)
+       (w/postwalk rewrite-for)
        (w/postwalk rewrite-tree-labels)
-       (w/postwalk strip-then-else)
+       (w/postwalk strip-control)
        (w/postwalk rewrite-line)
        (w/postwalk collapse-statements)
        (w/postwalk unnest-statements)
        (w/postwalk statementify)
-       (programmify)
-       ;; All parsed control forms have been rewritten to statements
-))
+       (programmify)))
 
-;; (defn postwalk-nonmap [t]
-;;   (cond (or (nil? t) (empty? t))
-;;         t
-;;         (or
-;;          (and (coll? t) (not (map? t)))
-;;          (seq? t))
-;;         (do
-;;           (postwalk-nonmap (first t))
-;;           (println "looking at: " t))))
-
-(defn process-conditionals [s]
-  (println "processing " s)
-  (ip/transform {:constant vector} s))
-  ;; (ip/transform {:goto #([])}
-   ;; {
-
-    ;;:if-clause   #(label-clause-level 0 %)
-    ;;:if-then       #(label-clause-level 0 %)
-    ;; :cond-destination (fn cond-to-goto [expression] {:action :goto :args [expression]})
-    ;; :then-clause      #(label-clause-level 1 [%])
-    ;; :else-clause      #(label-clause-level 2 [%])
-    ;; }
-;;    s)
-;; )
 (defn process [s]
   (-> s process-expressions))
+
+(defn parse [s]
+  (-> s
+      basic
+      process
+      proc-steps))
 
 (declare express)
 
@@ -450,6 +335,7 @@
       (println "type:" typ "a:" a "b:" b)
       (case typ
         nil       nil
+        :expression (express cxt a)
         :constant a
         :id       (get-in cxt [:vars a])
         :+        (+ (express cxt a) (express cxt b))
@@ -478,19 +364,42 @@
 (defn store-program [cxt program]
   (assoc cxt :program program))
 
-(defn action-jump [cxt dest-pair]
+(defn action-assign [cxt args]
+  (assoc-in cxt [:vars (second (first args))] (express cxt (fnext args))))
+
+
+(defn action-jump [cxt label]
+  (println "action-jump to" label)
   (-> cxt
-      (assoc-in [:ip] (avl/subrange (:program cxt) >= dest-pair))
+      (assoc-in [:ip] (avl/subrange (:program cxt) >= label))
       (assoc-in [:jumped?] true)))
 
-(defn action-goto [cxt args]
-  (let [dest-label (express cxt (first args))]
-    (action-jump [dest-label 0])))
+(defn action-goto [cxt [dest]]
+  (println "action-goto to" dest)
+  (let [dest-label (case (first dest)
+                     :label (second dest)
+                     [(express cxt dest)])]
+    (println "jumping to" dest-label)
+    (action-jump cxt dest-label)))
 
 (defn action-gosub [cxt args]
   (action-goto
    (-> (update-in cxt [:substack] #(conj % (next (:ip cxt)))))
    args))
+
+(defn action-store-for [cxt [[_ id] label end-expr step]]
+  (assoc-in cxt [:for-map id]
+            {:id id :label label :end-val end-expr :step step}))
+
+(defn action-next [cxt args]
+  ;; FIXME Update this for multiple arguments
+  (let [[_ id] (first args)
+        {:keys [label end-val step]} (get-in cxt [:for-map id])]
+    (-> cxt
+        (action-assign [[:id id] [:+ [:id id] step]])
+        (#(if (btrue? % [:<= [:id id] end-val])
+            (action-goto % [label])
+            cxt)))))
 
 (defn action-return [cxt _]
   (-> cxt
@@ -498,25 +407,27 @@
       (assoc-in [:jumped?] true)
       (update-in [:substack] pop)))
 
+(defn action-test-jump [cxt args]
+  (if (btrue? cxt (first args))
+    (action-goto cxt (second args))
+    cxt))
+
 (defn execute [cxt {:keys [action args] :as stmt}]
   (println "trying to execute " stmt)
   (println "action:" action "args:" args)
   (case action
-    :assignment
-    (assoc-in cxt [:vars (second (first args))] (express cxt (fnext args)))
-    :print  (do
-              (println "OUTPUT:" (apply express cxt args))
-              cxt)
-    :if     (if (btrue? cxt (first args))
-              (action-goto cxt [(second args)])
-              (if (= (count args) 3)
-                (action-goto cxt [(nth args 2)])
-                cxt))
-    :goto   (action-goto cxt args)
-    :gosub  (action-gosub cxt args)
-    :remark cxt
-    :return (action-return cxt args)
-    :end    (assoc-in cxt [:running?] false)))
+    :assignment (action-assign cxt args)
+    :print      (do
+                  (println "OUTPUT:" (apply express cxt args))
+                  cxt)
+    :test-jump  (action-test-jump cxt args)
+    :goto       (action-goto cxt args)
+    :gosub      (action-gosub cxt args)
+    :store-for  (action-store-for cxt args)
+    :next       (action-next cxt args)
+    :remark     cxt
+    :return     (action-return cxt args)
+    :end        (assoc-in cxt [:running?] false)))
 
 (defn interpret [cxt line]
   (let [ast (first (vals  (process (basic line))))]
@@ -535,7 +446,8 @@
                   (assoc :ip (:program cxt))
                   (assoc :running? true)
                   (assoc :jumped? false)
-                  (assoc :substack '()))]
+                  (assoc :substack '())
+                  (assoc :for-map {}))]
     (let [stmt (val (first (:ip cxt)))
           cxt  (->
                 (execute cxt stmt)
@@ -543,3 +455,9 @@
       (if (and (:running? cxt) (:ip cxt))
         (recur cxt)
         (-> cxt (dissoc :ip :running? :jumped? :substack))))))
+
+(defn srun [prog]
+  (pp/pprint (->> prog
+                  parse
+                  (store-program {})
+                  run)))
