cfaf7688a19be67fd8268d5a39bb60f667cf5af6
Paul Snyder <paul@pataprogramming.com>
2014-09-22 08:55:52 -0400

    Revert "Missed a change in the last commit"
    This reverts commit ec17fe856a55f6b25270cd4e7da37a1274b08016.

--- src/basic/repl.clj
+++ src/basic/repl.clj
@@ -38,6 +38,11 @@
 
 ;;;; Basic runners for REPL testing
 
+ (defn handle-parsed [cxt ast]
+  (case (first ast)
+    :program (store-program (action-reset cxt) ast)
+    :line    (store cxt ast)))
+
 
 (defn repl [cxt]
   (print "> ")
